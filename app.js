19b17807dd24484d8a2bfa51aee8cacfee93a101
Michael Yong <wrong92@gmail.com>
2012-03-30 00:04:22 +0800

    changed port number

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.get('/logout', function(req,res){
   });
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 9002;
 app.listen(port, function() {
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
