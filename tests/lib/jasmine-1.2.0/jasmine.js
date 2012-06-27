4f15dd56350c377bc2cc183abb8589e5e3e29904
Michael Yong <wrong92@gmail.com>
2012-06-27 17:36:56 +0800

    refine test runner to use original js files

--- tests/lib/jasmine-1.2.0/jasmine.js
+++ tests/lib/jasmine-1.2.0/jasmine.js
@@ -2525,6 +2525,5 @@ jasmine.version_= {
   "major": 1,
   "minor": 2,
   "build": 0,
-  "revision": 1333492179,
-  "release_candidate": 2
+  "revision": 1337005947
 };
--- tests/spec/test.js
+++ tests/spec/test.js
@@ -1,17 +1,19 @@
+/*global require:true describe it expect*/
 require.config({
+	baseUrl: "../public",
 	paths: {
 		'jquery': "http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min",
 		//'jquery': "libs/jquery-1.7.2",
-		'underscore': "../../js/libs/underscore",
-		'backbone': "../../js/libs/backbone",
-		'ich': "../../js/libs/icanhazamd",
-		'ivle': "../../js/libs/ivle",
-		'mainapp' : "../../js/scripts/app",
-		'appmodels' : "../../js/scripts/models",
-		'appviews' : "../../js/scripts/views",
+		'underscore': "js/libs/underscore",
+		'backbone': "js/libs/backbone",
+		'ich': "js/libs/icanhazamd",
+		'ivle': "js/libs/ivle",
+		'mainapp' : "js/scripts/app",
+		'appmodels' : "js/scripts/models",
+		'appviews' : "js/scripts/views"
 	}
 });
-require(["appmodels"], 
+require(["appmodels"],
 function(m){
 	//jasmine test.
 	describe("m.File", function(){
@@ -22,7 +24,5 @@ function(m){
 			expect(file.calcfilesize(1234)).toEqual("1.21 KB");
 			expect(file.calcfilesize(1323123)).toEqual("1.26 MB");
 		});
-		//clean up
-		delete file;
 	});
 });
